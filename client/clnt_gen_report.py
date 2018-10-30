582812f551b8bde76a7edea10b93219111ef1af8
pepsi7959 <narongsak.mala@gmail.com>
2018-10-30 17:06:22 +0700

    specifiy folder path

--- client/clnt_gen_report.py
+++ client/clnt_gen_report.py
@@ -7,7 +7,7 @@ Create by	: Narongsak Mala<narongsak.mala@dga.or.th>
 Email		: narongsak.mala@dga.or.th
 Description	: Client script for generating report
 '''
-
+import datetime
 import requests 
 import json
 
@@ -15,8 +15,8 @@ import json
 # reportId_str = '{"reportIds": [1,2,3,4]}';
 
 # URL
-allReportIdsURL = "http://localhost:8000/gsurvey/apis/allreports"
-genReportURL = "http://localhost:8000/gsurvey/apis/genreport/"
+allReportIdsURL = "http://localhost:8000/gsurvey/admin/apis/allreports"
+genReportURL = "http://localhost:8000/gsurvey/admin/apis/genReportAsExcel"
 
 # Queries all report 
 r = requests.get(allReportIdsURL);
@@ -27,16 +27,22 @@ print(r.json)
 reportIds = r.json()
 print(reportIds["reportIds"]);
 
+success_cnt = 0;
+total_cnt = 0;
 # Call each of reports
 for reportId in reportIds["reportIds"] :
+	total_cnt += 1 
 	print("report ID: "+reportId)
-	url = genReportURL + str(reportId)
+	url = genReportURL + "/" + str(reportId) + "/" + datetime.datetime.today().strftime('%Y%m%d_0020')
 	print("calling " + url)
 	r = requests.get(url) 
 	print("status: " + str(r.status_code))
 	
 	# Verify status
 	if r.status_code == 200 :
+		success_cnt += 1
 		print("generate report ID " + str(reportId) + " success")
 	else:
 		print("generate report ID " + str(reportId) + " failed")
+
+print("success: " + str(success_cnt) + " failed: " + str(total_cnt-success_cnt))
