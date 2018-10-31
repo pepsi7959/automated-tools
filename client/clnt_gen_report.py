1deef37d5acda1ba0221f769e91671329fd1229a
pepsi7959 <narongsak.mala@gmail.com>
2018-10-31 16:06:09 +0700

    support specific url

--- client/clnt_gen_report.py
+++ client/clnt_gen_report.py
@@ -10,39 +10,53 @@ Description	: Client script for generating report
 import datetime
 import requests 
 import json
+import sys
 
-# mock report 
-# reportId_str = '{"reportIds": [1,2,3,4]}';
+class ClientGenerator:
 
-# URL
-allReportIdsURL = "http://localhost:8000/gsurvey/admin/apis/allreports"
-genReportURL = "http://localhost:8000/gsurvey/admin/apis/genReportAsExcel"
+	# mock report 
+	# reportId_str = '{"reportIds": [1,2,3,4]}';
 
-# Queries all report 
-r = requests.get(allReportIdsURL);
-print(r.json)
+	# URL
+	allReportIdsURL = "http://localhost:8000/gsurvey/admin/apis/allreports"
+	genReportURL = "http://localhost:8000/gsurvey/admin/apis/genReportAsExcel"
 
-# Parses to json format
-#reportIds = json.loads(reportId_str); 
-reportIds = r.json()
-print(reportIds["reportIds"]);
+	def genReports(self, postfix, allReportIdsURL, genReportURL):
+		# Queries all report 
+		r = requests.get(allReportIdsURL);
+		print(r.json)
 
-success_cnt = 0;
-total_cnt = 0;
-# Call each of reports
-for reportId in reportIds["reportIds"] :
-	total_cnt += 1 
-	print("report ID: "+reportId)
-	url = genReportURL + "/" + str(reportId) + "/" + datetime.datetime.today().strftime('%Y%m%d_0020')
-	print("calling " + url)
-	r = requests.get(url) 
-	print("status: " + str(r.status_code))
+		# Parses to json format
+		#reportIds = json.loads(reportId_str); 
+		reportIds = r.json()
+		print(reportIds["reportIds"]);
+
+		success_cnt = 0;
+		total_cnt = 0;
+		# Call each of reports
+		for reportId in reportIds["reportIds"] :
+			total_cnt += 1 
+			print("report ID: "+reportId)
+			url = genReportURL + "/" + str(reportId) + "/" + datetime.datetime.today().strftime('%Y%m%d') + postfix
+			print("calling " + url)
+			r = requests.get(url) 
+			print("status: " + str(r.status_code))
 	
-	# Verify status
-	if r.status_code == 200 :
-		success_cnt += 1
-		print("generate report ID " + str(reportId) + " success")
-	else:
-		print("generate report ID " + str(reportId) + " failed")
+			# Verify status
+			if r.status_code == 200 :
+				success_cnt += 1
+				print("generate report ID " + str(reportId) + " success")
+			else:
+				print("generate report ID " + str(reportId) + " failed")
 
-print("success: " + str(success_cnt) + " failed: " + str(total_cnt-success_cnt))
+		print("success: " + str(success_cnt) + " failed: " + str(total_cnt-success_cnt))
+
+
+# Create Client Generator 
+clntGen = ClientGenerator()
+
+if len(sys.argv) > 3 :
+	clntGen.genReports(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+	print "Example command:" 
+	print "python ./clnt_gen_report.py _0200 http://localhost:8000/gsurvey/admin/apis/allreports http://localhost:8000/gsurvey/admin/apis/genReportAsExcel" 
