ff3da2f6a69192b84975e11f70c5281dfae5e43a
pepsi7959 <narongsak.mala@gmail.com>
2018-11-02 08:42:06 +0700

    Add exception management

--- client/clnt_gen_report.py
+++ client/clnt_gen_report.py
@@ -22,48 +22,63 @@ class ClientGenerator:
 	genReportURL = "http://localhost:8000/gsurvey/admin/apis/genReportAsExcel"
 
 	def genReports(self, postfix, allReportIdsURL, genReportURL):
-		# Queries all report 
-		r = requests.get(allReportIdsURL);
-		print(r.json)
-
-		# Parses to json format
-		#reportIds = json.loads(reportId_str); 
-		reportIds = r.json()
-		print(reportIds["results"]);
-
-		success_cnt = 0;
-		total_cnt = 0;
-		# Call each of reports
-		for report in reportIds["results"] :
-			total_cnt += 1 
-			reportId = report['GenerateID']
-			reportName = report['GenerateName']
-			reportIdName = "Report ID: " + str(reportId) + " Name: " + reportName
-			url = genReportURL + "/" + str(reportId) + "/" + datetime.datetime.today().strftime('%Y%m%d') + postfix
-			print(reportIdName + " calling " + url)
-			r = requests.get(url) 
-			print("status: " + str(r.status_code))
 	
-			# Verify status
-			if r.status_code == 200 :
-				response = r.json()
-				
-				if response["data"]["status"] != 0 : 
-					print(reportIdName + " failed: " + response["data"]["message"])
-				else :
-					success_cnt += 1
-					print(reportIdName + " success")
-			else:
-				print(reportIdName + " failed")
+		try:
+			# Queries all report 
+			r = requests.get(allReportIdsURL);
+			print(r.json)
+			
+			# Parses to json format
+			#reportIds = json.loads(reportId_str); 
+			reportIds = r.json()
+			print(reportIds["results"]);
+			success_cnt = 0;
+			total_cnt = 0;
+		
+			# Call each of reports
+			for report in reportIds["results"] :
+				total_cnt += 1 
+				reportId = report['GenerateID']
+				reportName = report['GenerateName']
+				reportIdName = "Report ID: " + str(reportId) + " Name: " + reportName
+				url = genReportURL + "/" + str(reportId) + "/" + datetime.datetime.today().strftime('%Y%m%d') + postfix
+				print(reportIdName + " calling " + url)
+				r = requests.get(url) 
+				print("status: " + str(r.status_code))
+	
+				# Verify status
+				if r.status_code == 200 :
+					response = r.json()
+					if response["data"]["status"] != 0 : 
+						print(reportIdName + " failed: " + response["data"]["message"])
+					else :
+						success_cnt += 1
+						print(reportIdName + " success")
+				else:
+					print(reportIdName + " failed")
+			print("success: " + str(success_cnt) + " failed: " + str(total_cnt-success_cnt))
+			return 0
+		except Exception as e:
+			print(str(e))
+			return 1
 
-		print("success: " + str(success_cnt) + " failed: " + str(total_cnt-success_cnt))
 
+	def help(self) :
+		print "Example command:" 
+		print "python ./clnt_gen_report.py _0200 http://localhost:8000/gsurvey/admin/apis/allreports http://localhost:8000/gsurvey/admin/apis/genReportAsExcel" 
+		 
 
 # Create Client Generator 
 clntGen = ClientGenerator()
-
+retry = 0
+maxRetry = 3
 if len(sys.argv) > 3 :
-	clntGen.genReports(sys.argv[1], sys.argv[2], sys.argv[3])
+	while retry < maxRetry :
+		if clntGen.genReports(sys.argv[1], sys.argv[2], sys.argv[3]) == 0 :
+			break	
+		else:
+			retry += 1
+			print("Retry :" + str(retry))
+			continue
 else:
-	print "Example command:" 
-	print "python ./clnt_gen_report.py _0200 http://localhost:8000/gsurvey/admin/apis/allreports http://localhost:8000/gsurvey/admin/apis/genReportAsExcel" 
+	clntGen.help()
